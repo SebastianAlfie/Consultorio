9db922089cfb12e464f6475c58df4b1273866378
Sebastián Alfie <alfie.sebastian96@gmail.com>
2022-11-11 19:08:53 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 let turno2 = "si"
 
-
-
-
 function pedirTurnos() {
     for (let turno = 13; turno <= 20; turno++) {
+        class Persona {
+            constructor(nombre, edad, email) {
+                this.nombre = nombre;
+                this.edad = edad;
+                this.email = email;
+            }
+        }
+
+        const persona1 = new Persona(prompt("¿Cuál es su nombre"), prompt("¿Qué edad tiene?"), prompt("¿Cuál es su email?"))
+
         while (turno2 === "si") {
-        edad = prompt("Ingrese su edad")
-         {
-            if (edad < 18) {
-                let nombre = prompt("Ingrese su nombre para pedir un turno para pediatría");
-                let mensaje = `Asiganado turno a las ${turno} horas, Nombre: ${nombre}`;
-                alert(mensaje)
-                break
-            }
-            if (edad >= 18) {
-                let nombre = prompt("Ingrese su nombre para signar un turno");
-                let mensaje = `Asiganado turno a las ${turno} horas Nombre: ${nombre}`;
-                alert(mensaje)
-                break
-            }
-            else {
-                alert ("Usted no ingreso un número")
+            edad = persona1.edad
+            {
+                if (edad < 18) {
+                    let nombre = persona1.nombre;
+                    let mensaje = `Asiganado turno para pediatria a las ${turno} horas, Nombre: ${nombre}, le enviaremos un email al siguiente correo electronico ${persona1.email}`;
+                    alert(mensaje)
+                    break
+                }
+                if (edad >= 18) {
+                    let nombre = persona1.nombre;
+                    let mensaje = `Asiganado turno a las ${turno} horas, Nombre: ${nombre}, le enviaremos un email al siguiente correo electronico ${persona1.email}`;
+                    alert(mensaje)
+                    break
+                }
+                else {
+                    alert("Usted no ingreso un número")
                 }
             }
         }
-        turno2 = prompt("Si quiere pedir otro turno, escribir si").toLowerCase()
+        turno2 = prompt("Si quiere pedir otro turno, escribir si para continuar o no para salir").toLowerCase()
+        if (turno2 === "no") {
+
+            break;
+
+        }
+
     }
 }
 pedirTurnos()
